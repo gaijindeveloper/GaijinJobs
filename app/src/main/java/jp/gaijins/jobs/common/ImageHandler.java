04b84fc5f5b6d2dc1336e195f0c0d3175d1dbc66
vishy <vishy.nyk.gaijin@gmail.com>
2015-12-21 14:21:47 +0900

    test commit

--- app/src/main/java/jp/gaijins/jobs/common/ImageHandler.java
+++ app/src/main/java/jp/gaijins/jobs/common/ImageHandler.java
@@ -10,7 +10,6 @@ import java.util.concurrent.Executors;
  * Created by nayak.vishal on 2015/12/10.
  */
 public class ImageHandler {
-
     private static Picasso instance;
 
     public static Picasso getSharedInstance(Context context) {
